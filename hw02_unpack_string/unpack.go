505b3cc33cddf44964de423a712ce3d97c5cfe74
ameyuuno <ameyuuno@gmail.com>
2021-11-29 19:00:41 +0300

    implement unpacking string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	unpackedString := strings.Builder{}
+
+	var previousSymbol rune
+	for _, currentSymbol := range str {
+		if previousSymbol == 0 {
+			if unicode.IsDigit(currentSymbol) {
+				return "", ErrInvalidString
+			}
+			previousSymbol = currentSymbol
+			continue
+		}
+
+		if !unicode.IsDigit(currentSymbol) {
+			unpackedString.WriteRune(previousSymbol)
+			previousSymbol = currentSymbol
+			continue
+		}
+
+		repetitionNumber, err := strconv.Atoi(string(currentSymbol))
+		if err != nil {
+			return "", ErrInvalidString
+		}
+
+		unpackedString.WriteString(strings.Repeat(string(previousSymbol), repetitionNumber))
+		previousSymbol = 0
+	}
+
+	if previousSymbol != 0 {
+		unpackedString.WriteRune(previousSymbol)
+	}
+
+	return unpackedString.String(), nil
 }
